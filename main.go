9f321307502683a8242ecf47bad97c4c5da21792
dddddO <lbfdeatq@gmail.com>
2018-12-31 01:23:34 +0900

    #1 add command ifconfig

--- main.go
+++ main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/exec"
 )
 
 func main() {
-	fmt.Println("aaaaa")
+	//cmd := exec.Command("ls", "-al")
+	c := "ifconfig"
+	cmd := exec.Command(c)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("failed to command '%s': %v\n", c, err)
+	}
+
+	var w []byte
+	_, err := cmd.Stdout.Write(w)
+	if err != nil {
+		log.Fatalln("failed to stdout write '%s': %v\n", c, err)
+	}
+
+	fmt.Print(string(w))
+
 }
