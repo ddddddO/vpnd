4d4743d05cee1620b42deef75184fc32c4f9f170
dddddO <lbfdeatq@gmail.com>
2018-12-31 10:46:42 +0900

    fix exec command prosc

--- main.go
+++ main.go
@@ -1,28 +1,43 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
 	"log"
-	"os"
 	"os/exec"
+	"strings"
 )
 
 func main() {
-	//cmd := exec.Command("ls", "-al")
-	c := "ifconfig"
-	cmd := exec.Command(c)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	if err := cmd.Run(); err != nil {
-		log.Fatalf("failed to command '%s': %v\n", c, err)
+	var (
+		command = "ifconfig"
+		target = "wifi0" // TODO:実行環境の設定に変更
+		cmd = exec.Command(command, target)
+		stdout, _ = cmd.StdoutPipe()
+		//stdin, _ = cmd.StdinPipe()
+		//stderr, _ = cmd.StderrPipe()
+	)
+	if err := cmd.Start(); err != nil {
+		log.Fatalf("failed to command '%s': %v\n", command, err)
 	}
 
-	var w []byte
-	_, err := cmd.Stdout.Write(w)
-	if err != nil {
-		log.Fatalln("failed to stdout write '%s': %v\n", c, err)
+	var (
+		sc = bufio.NewScanner(stdout)
+		ip = "192.168.179.4"
+		netmask = "255.255.255.0"
+		ifconFlg = false 
+	)
+	for sc.Scan() {
+		s := sc.Text()
+		if strings.Contains(s, ip) && strings.Contains(s, netmask) {
+			log.Println("in!!!")
+			ifconFlg = true
+			break
+		}
 	}
+	cmd.Wait()
 
-	fmt.Print(string(w))
-
+	if !ifconFlg {
+		// 仮想LANカードにグローバルIPを設定する処理
+		log.Println("to set g ip")
+	}
 }
